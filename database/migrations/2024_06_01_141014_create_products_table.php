66b6ffeb3fa624ef5cb02a051a7cce241b18414b
kevinnaserwan <kevinnaserwan@gmail.com>
2024-06-01 21:17:53 +0700

    Feat: add table cart, likes and products

--- database/migrations/2024_06_01_141014_create_products_table.php
+++ database/migrations/2024_06_01_141014_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('photo')->nullable();
             $table->string('material');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
